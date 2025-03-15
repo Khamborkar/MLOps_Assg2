543b3c793c72add2ea3069185d6f2458028f14ec
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-03-15 22:49:52 +0900

    Update monitoring.py

--- src/monitoring.py
+++ src/monitoring.py
@@ -4,6 +4,7 @@ import mlflow.sklearn
 from sklearn.metrics import accuracy_score
 import tensorflow
 from tensorflow.keras.datasets import fashion_mnist
+import pandas as pd
 
 # Load Fashion MNIST dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
