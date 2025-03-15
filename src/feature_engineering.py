9b04decfb7e3f80b62bf7e6248fae9c7702bbeef
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-03-15 18:47:29 +0900

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -5,6 +5,8 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
+import numpy
+import pandas
 
 # Feature Engineering & Explainability function
 def feature_engineering(train_df):
