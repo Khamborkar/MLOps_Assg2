a66c1bd2caaefb7b0928090ab4ab48e71d9ec99f
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-03-15 19:32:07 +0900

    Update eda.py

--- src/eda.py
+++ src/eda.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 import sweetviz as sv
 import numpy
+import main.py
 
 # EDA function that accepts DataFrame
 def generate_eda_report(train_df):
